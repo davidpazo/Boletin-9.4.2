03af1a60d59f729740fa863559097742e484aedf
dpazolopez <dpazolopez@eniac21i.danielcastelao.org>
2014-11-20 11:03:57 +0100

    finish

---
+++ src/Boletin94/Boletin94.java
@@ -0,0 +1,11 @@
+package Boletin94;
+/** @author dpazolopez
+ */
+public class Boletin94 {
+
+    public static void main(String[] args) {
+        Tabla obx=new Tabla();
+        obx.producto();
+    }
+    
+}
